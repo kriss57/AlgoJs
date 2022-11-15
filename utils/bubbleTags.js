c55dda8507a2d7c273b82c9822658dbfc642eb6b
chris <antit571@sfr.fr>
2022-11-15 17:34:31 +0100

    bubble tag display preparation

--- utils/bubbleTags.js
+++ utils/bubbleTags.js
@@ -5,5 +5,11 @@ const bubble = document.querySelectorAll('bubble_tag')
 console.log(bubble);
 
 const displayBubbleTags = () => {
+    //afficher les tags
 
+}
+
+const resetDisplayerBubbleTags = () {
+    //suprimmer les tags
+    //reset les cards
 }
