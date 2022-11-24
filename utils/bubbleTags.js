01aac23acc6f3b932b617e640e356e56f67b0d7c
chris <antit571@sfr.fr>
2022-11-24 11:56:16 +0100

    tag and dropdown management

--- utils/bubbleTags.js
+++ utils/bubbleTags.js
@@ -4,16 +4,43 @@
 const bubbleContainer = document.querySelector('.bubble_container')
 const deleteBtn = document.querySelectorAll('.bubble_closed')
 
+const displayBubbleTags = (e) => {
 
-const displayBubbleTags = (tagName) => {
+    // pointeur dom pour gestion dropdown
+    let containeur = e.target.parentNode.parentNode
+    let dropdown = e.target.parentNode.parentNode.parentNode
+    let arrowBtn = e.target.parentNode.parentNode.parentNode.childNodes[1].children[1]
+
+
+    let listName = e.target.parentNode.id
+    let tagName = e.target.innerText
+    let className
+
+    if (listName == 'ingredients-list') {
+        className = 'bubble_tag_ingredients'
+        toggleDropdown(containeur, dropdown, arrowBtn)
+
+    }
+    if (listName == 'appareils-list') {
+        className = 'bubble_tag_appareils'
+        toggleDropdown(containeur, dropdown, arrowBtn)
+
+    }
+    if (listName == 'ustensiles-list') {
+        className = 'bubble_tag_ustensiles'
+        toggleDropdown(containeur, dropdown, arrowBtn)
+
+    }
 
     let bubbleTemplate = `
-         <div class="bubble_tag" id="${tagName}-tag">
+         <div class="${className}" >
                 <p class="bubble_text">${tagName}</p>
                 <img class="bubble_closed" src="./assets/icons/closed.svg" alt="bouton de fermeture">
             </div>
     `
     bubbleContainer.insertAdjacentHTML('beforeend', bubbleTemplate)
+
+
 }
 
 // ------------------------------------------------------------------------ //
--- utils/dropdown.js
+++ utils/dropdown.js
@@ -1,8 +1,12 @@
 // ---- Pointer Dom
-//dropdown
+// dropdown
 const ingredientsDropdown = document.querySelector('#first-dropdown')
 const appareilsDropdown = document.querySelector('#secondary-dropdown')
 const ustensilesDropdown = document.querySelector('#third-dropdown')
+// ArrowBtn
+const ingredientsBtn = document.getElementById('ingredientsBtn')
+const appareilsBtn = document.getElementById('appareilsBtn')
+const ustensilesBtn = document.getElementById('ustensilesBtn')
 
 const arrowBtn = document.querySelectorAll('.arrow-down')
 //containeur
@@ -20,47 +24,63 @@ let isVisible = false
 /**
  * Ouverture et fermeture du menu de tri
  */
-const toggleDropdown = (container, dropdown) => {
+const toggleDropdown = (container, dropdown, arrowBtn) => {
     //listBottom.classList.toggle("isVisible")  // method toggle()
     isVisible = !isVisible  // version ternere
     isVisible ? container.classList.add("isVisible") : container.classList.remove("isVisible")
     isVisible ? dropdown.classList.add("resize") : dropdown.classList.remove("resize")
-    //isVisible ? arrowBtn.classList.add("rotate") : arrowBtn.classList.remove("rotate")
+    isVisible ? arrowBtn.classList.add("rotate") : arrowBtn.classList.remove("rotate")
 
 }
-for (let i = 0; i < arrowBtn.length; i++) {
-    arrowBtn[i].addEventListener('click', (e) => {
-        // console.log(e.target.parentNode.id);
-        if (e.target.parentNode.id === 'ingredientsBtn') {
-            toggleDropdown(ingredientsContainer, ingredientsDropdown)
-            resetDropdown(appareilsDropdown, appareilsContainer)
-            resetDropdown(ustensilesDropdown, ustensilesContainer)
 
-        }
-        if (e.target.parentNode.id === 'appareilsBtn') {
-            toggleDropdown(appareilsContainer, appareilsDropdown)
-            resetDropdown(ingredientsDropdown, ingredientsContainer)
-            resetDropdown(ustensilesDropdown, ustensilesContainer)
+// ------------------------------------------
+// -- bascule d'un dropdown a un autre
+ingredientsBtn.addEventListener('click', (e) => {
+    toggleDropdown(ingredientsContainer, ingredientsDropdown, ingredientsBtn)
+    resetDropdown(appareilsDropdown, appareilsContainer, appareilsBtn)
+    resetDropdown(ustensilesDropdown, ustensilesContainer, ustensilesBtn)
+
+})
+appareilsBtn.addEventListener('click', (e) => {
+    toggleDropdown(appareilsContainer, appareilsDropdown, appareilsBtn)
+    resetDropdown(ingredientsDropdown, ingredientsContainer, ingredientsBtn)
+    resetDropdown(ustensilesDropdown, ustensilesContainer, ustensilesBtn)
+})
+ustensilesBtn.addEventListener('click', (e) => {
+    toggleDropdown(ustensilesContainer, ustensilesDropdown, ustensilesBtn)
+    resetDropdown(ingredientsDropdown, ingredientsContainer, ingredientsBtn)
+    resetDropdown(appareilsDropdown, appareilsContainer, appareilsBtn)
+})
 
 
-        }
-        if (e.target.parentNode.id === 'ustensilesBtn') {
-            toggleDropdown(ustensilesContainer, ustensilesDropdown)
-        }
-    })
-}
-const resetDropdown = (dropdown, container) => {
+// for (let i = 0; i < arrowBtn.length; i++) {
+//     arrowBtn[i].addEventListener('click', (e) => {
+//         console.log(e.target.parentNode.id);
+//         if (e.target.parentNode.id === 'ingredientsBtn') {
+//             toggleDropdown(ingredientsContainer, ingredientsDropdown, ingredientsBtn)
+//             resetDropdown(appareilsDropdown, appareilsContainer, appareilsBtn)
+//             resetDropdown(ustensilesDropdown, ustensilesContainer, ustensilesBtn)
+
+//         }
+//         if (e.target.parentNode.id === 'appareilsBtn') {
+//             toggleDropdown(appareilsContainer, appareilsDropdown, appareilsBtn)
+//             resetDropdown(ingredientsDropdown, ingredientsContainer, ingredientsBtn)
+//             resetDropdown(ustensilesDropdown, ustensilesContainer, ustensilesBtn)
+
+
+//         }
+//         if (e.target.parentNode.id === 'ustensilesBtn') {
+//             toggleDropdown(ustensilesContainer, ustensilesDropdown, ustensilesBtn)
+//             resetDropdown(ingredientsDropdown, ingredientsContainer, ingredientsBtn)
+//             resetDropdown(appareilsDropdown, appareilsContainer, appareilsBtn)
+//         }
+//     })
+// }
+const resetDropdown = (dropdown, container, arrowBtn) => {
     dropdown.classList.remove("resize")
     container.classList.remove("isVisible")
+    arrowBtn.classList.remove("rotate")
 }
-const timerDropdown = () => {
-    // ajout timer 
-    if (isVisible) {
-        window.setTimeout(() => {
-            container.classList.remove("isVisible")
-            //arrowImg.classList.remove("rotate")
-        }, 10000)
-    }
-}
+
 
 
